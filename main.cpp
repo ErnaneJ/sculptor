9c90991d75c6a5d5cd917a939db42cbc999dea94
Ernane Ferreira <ernane.junior25@gmail.com>
2022-05-18 18:32:36 -0300

    Add example caracter E

--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 #include "./headers/sculptor.h"
 
 int main() {
-  Sculptor Placa(100,75,100);
-  Sculptor trono(10,10,10);
-  trono.setColor(0,0,1.0,1.0);
-  trono.putBox(0,9,0,9,0,9);
-  trono.cutBox(1,8,1,9,1,9);
-  trono.writeOFF("off_files/trono.off");
+  Sculptor letra_e(10,10,10);
+  letra_e.setColor(0,0,1.0,1.0);
+  letra_e.putBox(0,9,0,2,0,2);
+  letra_e.putBox(0,5,2,4,0,2);
+  letra_e.putBox(0,9,4,6,0,2);
+  letra_e.putBox(0,5,6,8,0,2);
+  letra_e.putBox(0,9,8,10,0,2);
+  // letra_e.cutBox(1,8,1,9,1,9);
+  letra_e.writeOFF("off_files/e.off");
 }
