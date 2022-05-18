9371d742442a5f1f98ee00b6833b49b09ddf6327
Ernane Ferreira <ernane.junior25@gmail.com>
2022-05-18 17:29:54 -0300

    automating compilation and execution

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Running
+
+```bash
+  $ chmod +x ./run.sh && ./run.sh
+
+  # to directly open the file in geomeview
+
+  $ chmod +x ./run.sh && ./run.sh --geomview trono
+```
---
+++ run.sh
@@ -0,0 +1,15 @@
+declare -a files=(`ls -d -1 ./**/*.cpp`)
+
+echo "🟢 Running: g++ ./main.cpp ${files[*]} -o ./main && ./main"
+echo 
+
+`g++ $PWD/main.cpp ${files[*]} -o $PWD/main && $PWD/main`
+
+if [ "$1" = "--geomview" ]; then
+  if [ "$2" != "" ]; then
+    echo "📁 Opening file \"./off_files/$2.off\" in geomview."
+    `geomview $PWD/off_files/$2.off`
+  else
+    echo "🔴 [Error] Flag --geomeview needs to be passed then the name of the file to be opened."
+  fi
+fi
