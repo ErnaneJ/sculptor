c256749f8eee5a8bf5432683766d025d2ca24f6e
Ernane Ferreira <ernane.junior25@gmail.com>
2022-05-28 22:20:14 -0300

    Add docs files for github pages

---
+++ docs/classSculptor.js
@@ -0,0 +1,23 @@
+var classSculptor =
+[
+    [ "Sculptor", "classSculptor.html#a014e3ef5517bf0e9d9e14486b6ac6433", null ],
+    [ "~Sculptor", "classSculptor.html#a8f159bf97458326f16d2e238e11be7ff", null ],
+    [ "cutBox", "classSculptor.html#aa84a1b12b09e9e103fc8d78f8d1bc00f", null ],
+    [ "cutEllipsoid", "classSculptor.html#a18d2922c111c4c13653ee07d878151ad", null ],
+    [ "cutSphere", "classSculptor.html#a67ab8c0ba5116adb8af1d01ad373ac15", null ],
+    [ "cutVoxel", "classSculptor.html#ad9d714a35fc8ae16d06eb5df37c3493c", null ],
+    [ "putBox", "classSculptor.html#a311ad7a0fb83fc67ac1f378be8e99fe1", null ],
+    [ "putEllipsoid", "classSculptor.html#a093615b0c2b9b3a17a56300b9b939f39", null ],
+    [ "putSphere", "classSculptor.html#a794a2b6ee8fc8098fd6150cb46101fc6", null ],
+    [ "putVoxel", "classSculptor.html#a4bdea3048b419d58e93074060eaa7b52", null ],
+    [ "setColor", "classSculptor.html#af1d69da01379874b0dfd6454787cb562", null ],
+    [ "writeOFF", "classSculptor.html#a58cb72d22001a5034f15383ca983830c", null ],
+    [ "a", "classSculptor.html#a6fd0157dcf17582f0edd5fddf157604e", null ],
+    [ "b", "classSculptor.html#a7aafd7305ea634252d8288b60536cd96", null ],
+    [ "g", "classSculptor.html#a208c06af69a81a1568df4493868816f1", null ],
+    [ "nx", "classSculptor.html#ad1e32f9042538419a3bc7b376f7813b8", null ],
+    [ "ny", "classSculptor.html#a1ce2ff97ec94927928ab3f5ec4ba6761", null ],
+    [ "nz", "classSculptor.html#a33204e7df26a7ee4c7192381a24335d3", null ],
+    [ "r", "classSculptor.html#a3f5d2ec3b66d645019b8d81c810a1cd8", null ],
+    [ "v", "classSculptor.html#a4ca53a2f2fbf41ca42dfe729ebe693f1", null ]
+];
---
+++ docs/dir_08d237fc27d4ecd563f71c5d52f2fecc.js
@@ -0,0 +1,4 @@
+var dir_08d237fc27d4ecd563f71c5d52f2fecc =
+[
+    [ "sculptor.cpp", "sculptor_8cpp.html", null ]
+];
